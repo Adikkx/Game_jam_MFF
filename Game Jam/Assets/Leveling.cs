88110c527b253a3c7a94731605dafae814168a76
kikaaa151 <kikulasuvakova@gmail.com>
2023-05-07 12:21:01 +0200

    turururur level count

--- Game Jam/Assets/Leveling.cs
+++ Game Jam/Assets/Leveling.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class Leveling : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public static int Level=1;
+    
 }
