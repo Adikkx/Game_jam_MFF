52ad320addf592d37735dcc57b97bff7996f14cb
kikaaa151 <kikulasuvakova@gmail.com>
2023-05-07 17:35:48 +0200

    da

--- Game Jam/Assets/MainMenu.cs
+++ Game Jam/Assets/MainMenu.cs
@@ -11,7 +11,7 @@ public class MainMenu : MonoBehaviour
     }
 
     public void Pause(){
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+2);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex-1);
 
     }
     public void Quit()
